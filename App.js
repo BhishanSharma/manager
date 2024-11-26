002590b36f47225ddc144ea6bd5c7f7bef979b8e
BhishanSharma <bhishansharma2@gmail.com>
2024-11-26 12:06:02 +0530

    local-storage

--- App.js
+++ App.js
@@ -2,16 +2,34 @@ const theme = document.getElementById("theme");
 const inTask = document.getElementById("inTask");
 const panel = document.querySelector(".getInput");
 const taskList = document.querySelector(".tasks");
-const close = document.getElementById("close");
+const closeBtn = document.getElementById("close");
 const outerAdder = document.getElementById("outerAdder");
 
-let dark_theme = 0;
+let dark_theme = localStorage.getItem("dark_theme") === "1" ? 1 : 0;
+
+const applyTheme = () => {
+  const rootElement = document.documentElement;
+  if (dark_theme) {
+    rootElement.classList.add("dark-theme");
+    theme.src = "../assets/Sun.png";
+    closeBtn.src = "../assets/CloseL.png";
+  } else {
+    rootElement.classList.remove("dark-theme");
+    theme.src = "../assets/Moon and Stars.png";
+    closeBtn.src = "../assets/Close.png";
+  }
+};
+applyTheme();
 
 const toggleTheme = () => {
   const rootElement = document.documentElement;
   rootElement.classList.toggle("dark-theme");
   dark_theme = dark_theme ? 0 : 1;
+
+  localStorage.setItem("dark_theme", dark_theme);
+
   theme.src = dark_theme ? "../assets/Sun.png" : "../assets/Moon and Stars.png";
+  closeBtn.src = dark_theme ? "../assets/CloseL.png" : "../assets/Close.png";
 };
 
 let tasks = [];
@@ -19,14 +37,23 @@ let tasks = [];
 inTask.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     const now = new Date();
-    tasks.push({ time: `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`, task: inTask.value, done: false });
+    let time = `${String(now.getHours()).padStart(2, "0")}:${String(
+      now.getMinutes()
+    ).padStart(2, "0")}`;
+
+    tasks.push({
+      date: now.toDateString(),
+      time: time,
+      task: inTask.value,
+      done: false,
+    });
     inTask.value = "";
     panel.style.display = "none";
     updateTasksList();
   }
 });
 
-close.addEventListener("click", () => {
+closeBtn?.addEventListener("click", () => {
   panel.style.display = "none";
 });
 
@@ -46,11 +73,11 @@ if (tasks.length == 0) {
   taskList.appendChild(noItemDiv);
 
   const add = document.querySelectorAll(".addFile");
-  add.forEach((button)=>{
-    button.addEventListener("click",()=>{
+  add.forEach((button) => {
+    button.addEventListener("click", () => {
       panel.style.display = "flex";
-    })
-  })
+    });
+  });
 }
 
 const updateTasksList = () => {
@@ -58,6 +85,7 @@ const updateTasksList = () => {
   taskList.innerHTML = "";
 
   tasks.forEach((task) => {
+    console.log(task.date);
     const taskDiv = document.createElement("div");
     taskDiv.classList.add("task");
     taskDiv.innerHTML = `
@@ -82,5 +110,5 @@ const updateTasksList = () => {
       taskStroke.style.display = "block";
       task.done = true;
     });
-  }); 
+  });
 };
