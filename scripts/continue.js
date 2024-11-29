35616007c5b7490290751db3b0e7f78faaf9eba8
BhishanSharma <bhishansharma2@gmail.com>
2024-11-29 13:51:30 +0530

    structural-changes

---
+++ scripts/continue.js
@@ -0,0 +1,7 @@
+let cont = document.querySelector(".submit");
+cont.addEventListener("click", () => {
+  window.location.href = "../home.html";
+});
+document.querySelector(".reg").addEventListener("click", () => {
+  window.location.href = "start.html";
+});
---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+let start = document.querySelector(".new");
+start.addEventListener("click", () => {
+  window.location.href = "public/auth/start.html";
+});
+
+let cont = document.querySelector(".cont");
+cont.addEventListener("click", () => {
+  window.location.href = "public/auth/continue.html";
+});
